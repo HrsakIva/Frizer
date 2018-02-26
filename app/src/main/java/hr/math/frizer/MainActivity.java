d78793c66421071750fcd5600f6a64890ab1f93e
Iva Hrsak <hrsakiva18@gmail.com>
2018-02-26 14:10:26 +0100

    Dodan menu

---
+++ app/src/main/java/hr/math/frizer/MainActivity.java
@@ -0,0 +1,67 @@
+package hr.math.frizer;
+
+import android.content.Intent;
+import android.support.design.widget.NavigationView;
+import android.support.v4.view.GravityCompat;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity
+
+        implements NavigationView.OnNavigationItemSelectedListener  {
+
+    private DrawerLayout mDrawerLayout;
+    private ActionBarDrawerToggle mToggle;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        mDrawerLayout = (DrawerLayout) findViewById(R.id.drawerlayout);
+        mToggle = new ActionBarDrawerToggle(this, mDrawerLayout, R.string.open, R.string.close);
+
+        mDrawerLayout.addDrawerListener(mToggle);
+        mToggle.syncState();
+
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+        NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
+        navigationView.setNavigationItemSelectedListener(this);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item){
+        if(mToggle.onOptionsItemSelected(item)){
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+
+   }
+
+    @Override
+    public boolean onNavigationItemSelected(MenuItem item) {
+        // Handle navigation view item clicks here.
+        int id = item.getItemId();
+
+        if (id == R.id.nav_pretrazi) {
+            Toast.makeText(this, "Camera", Toast.LENGTH_SHORT).show();
+        }
+        else if (id == R.id.nav_login) {
+            Intent i = new Intent(this, LoginActivity.class);
+            startActivity(i);
+        }
+
+
+
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawerlayout);
+        drawer.closeDrawer(GravityCompat.START);
+        return true;
+    }
+}
